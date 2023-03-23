0755189c8ae1db201c4fe5cd26cec133e6b63bb5
Mr.Phoenix <nieva.cronos@gmail.com>
2023-03-23 16:57:55 -0300

    Dockerfile

---
+++ src/main/java/com/portfolio/FernandoNieva/FernandoNievaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.FernandoNieva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandoNievaApplication {
+
+		public static void main(String[] args) {
+		SpringApplication.run(FernandoNievaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/FernandoNieva/FernandoNievaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.FernandoNieva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FernandoNievaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
